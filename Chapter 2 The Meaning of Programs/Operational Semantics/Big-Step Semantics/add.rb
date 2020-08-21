d3d43a8801deeeed98abc20a79ad0dc35d72c416
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-21 11:03:28 +0800

    Big-step rules for binary expressions Add, Multiply, and LessThan
    Requiring recursive evaluation of their left and right subexpressions
    before combining both values with the appropriate Ruby operator.
    
    $ irb --simple-prompt
    >> require './number.rb'
    >> require './boolean.rb'
    >> require './variable.rb'
    >> require './add.rb'
    >> require './multiply.rb'
    >> require './less_than.rb'
    
    >> Number.new(23).evaluate({})
    => <23>
    
    >> Variable.new(:x).evaluate({ x: Number.new(23) })
    => <23>
    
    >> LessThan.new(
    ?>  Add.new(Variable.new(:x), Number.new(2)),
    ?>  Variable.new(:y)
    >> ).evaluate({ x: Number.new(2), y: Number.new(5) })
    => <true>

---
+++ Chapter 2 The Meaning of Programs/Operational Semantics/Big-Step Semantics/add.rb
@@ -0,0 +1,13 @@
+class Add < Struct.new(:left, :right)
+  def to_s
+    "#{left} + #{right}"
+  end
+
+  def inspect
+    "<#{self}>"
+  end
+
+  def evaluate(environment)
+    Number.new(left.evaluate(environment).value + right.evaluate(environment).value)
+  end
+end
