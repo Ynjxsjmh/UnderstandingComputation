d1d0dc04fe26817ec1fc8c859fa87e4175abf6e7
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-21 20:46:54 +0800

    Big-step evaluation of an assignment statement
    We can think of bigstep statement evaluation as a process that always
    turns a statement and an initial environment into a final environment,
    avoiding the small-step complication of also having to deal with the
    intermediate statement generated by #reduce.
    
    Big-step evaluation of an assignment statement, for example, should
    fully evaluate its expression and return an updated environment
    containing the resulting value.

---
+++ Chapter 2 The Meaning of Programs/Operational Semantics/Big-Step Semantics/assign.rb
@@ -0,0 +1,13 @@
+class Assign < Struct.new(:name, :expression)
+  def to_s
+    "#{name} = #{expression}"
+  end
+
+  def inspect
+    "<#{self}>"
+  end
+
+  def evaluate(environment)
+    environment.merge({name => expression.evaluate(environment) })
+  end
+end
