7216d207f0b61c3ae48d2aa04975025754fe5a50
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-08-23 17:10:03 +0800

    The denotational semantics of sequence statement
    As in big-step operational semantics, we need to be careful about
    specifying the sequence statement: the result of evaluating the first
    statement is used as the environment for evaluating the second.

---
+++ Chapter 2 The Meaning of Programs/Denotational Semantics/sequence.rb
@@ -0,0 +1,13 @@
+class Sequence < Struct.new(:first, :second)
+  def to_s
+    "#{first}; #{second}"
+  end
+
+  def inspect
+    "<#{self}>"
+  end
+
+  def to_ruby
+    "-> e { (#{second.to_ruby}).call( (#{first.to_ruby}).call(e) ) }"
+  end
+end
