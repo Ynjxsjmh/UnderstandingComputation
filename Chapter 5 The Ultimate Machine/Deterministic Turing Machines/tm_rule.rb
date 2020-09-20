14459604d4631e77c0379cac4b11e2d0ab048d43
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-09-20 10:06:44 +0800

    Add the ability to update that configuration
    Once we know that a rule applies to a particular configuration, we
    need the ability to update that configuration by writing a new
    character, moving the tape head, and changing the machine’s state in
    accordance with the rule.
    
    That code seems to work fine:
    $ irb --simple-prompt
    
    >> require './tape.rb'
    >> require './tm_rule.rb'
    >> require './tm_configuration.rb'
    
    >> rule = TMRule.new(1, '0', 2, '1', :right)
    
    >> rule.follow(TMConfiguration.new(1, Tape.new([], '0', [], '_')))
    => #<struct TMConfiguration state=2, tape=#<Tape 1(_)>>

--- Chapter 5 The Ultimate Machine/Deterministic Turing Machines/tm_rule.rb
+++ Chapter 5 The Ultimate Machine/Deterministic Turing Machines/tm_rule.rb
@@ -3,4 +3,19 @@ class TMRule < Struct.new(:state, :character, :next_state,
   def applies_to?(configuarion)
     state == configuarion.state && character == configuarion.tape.middle
   end
+
+  def follow(configuarion)
+    TMConfiguration.new(next_state, next_tape(configuarion))
+  end
+
+  def next_tape(configuarion)
+    written_tape = configuarion.tape.write(write_character)
+
+    case direction
+    when :left
+      written_tape.move_head_left
+    when :right
+      written_tape.move_head_right
+    end
+  end
 end
